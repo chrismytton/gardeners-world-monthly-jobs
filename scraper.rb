2b6d3154bcd2414e3b6e42f696999429cdfdc2eb
Chris Mytton <chrismytton@gmail.com>
2020-03-29 07:47:45 +0100

    Simplify GET requests with open-uri

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,6 @@
 require 'date'
 require 'nokogiri'
-require 'net/http'
+require 'open-uri'
 require 'json'
 
 urls = (1..12).map do |month_number|
@@ -12,18 +12,12 @@ out = {}
 
 urls.each do |month, url|
   warn "Fetching: #{url}"
-  uri = URI(url)
-
-  Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-    request = Net::HTTP::Get.new uri
-    response = http.request request # Net::HTTPResponse object
-    page = Nokogiri::HTML(response.body)
-    out[month] ||= {}
-    page.css('.container > .editor-content').drop(2).take(4).each do |checklist|
-      section = checklist.css('h4').text.strip
-      tasks = checklist.css('li').map(&:text).map(&:strip)
-      out[month][section] = tasks
-    end
+  page = Nokogiri::HTML(URI.open(url).read)
+  out[month] ||= {}
+  page.css('.container > .editor-content').drop(2).take(4).each do |checklist|
+    section = checklist.css('h4').text.strip
+    tasks = checklist.css('li').map(&:text).map(&:strip)
+    out[month][section] = tasks
   end
 end
 
